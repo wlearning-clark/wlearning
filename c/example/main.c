d3bfe14d90f31b8af35307d2ed43a3b188d121b4
Clark W. Howard <wlearning.clark@gmail.com>
2026-05-21 07:46:59 +0800

    chore: add a simple template for c

---
+++ c/example/main.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/cdefs.h>
+#include <unistd.h>
+
+int main(__attribute_maybe_unused__ int argc, __attribute_maybe_unused__ char** argv)
+{
+  printf("Hello World.\n");
+  return (EXIT_SUCCESS);
+}
